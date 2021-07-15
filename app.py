f5276061a1066a442e2bc0097d2a1693a595cebd
nate-moo <40650681+nate-moo@users.noreply.github.com>
2021-07-15 02:15:22 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+from random import random
+from typing import List
+import requests
+from threading import Thread
+from flask import request, render_template, redirect, url_for, session, Flask
+from flask_cors import CORS
+import json
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+
+que = Queue()
+app = Flask(__name__)
+CORS(app)
+app.config["DEBUG"] = True
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+# route for main page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api")
+def api():
+    # with ThreadPoolExecutor() as executor:
+    #     future = [executor.submit(randIMG) for x in range(10)]
+    #     returnVal = [f.result() for f in future]
+    #     return str(json.dumps(returnVal))
+    # que = Queue()
+    # requestFast(10)
+    # #randIMG()
+    # result = que.get()
+    return str(json.dumps(requestFast(49)))
+
+
+def randIMG(ret):
+    imgList = str()
+    imgStr = list()
+    if random() > 0.2:
+        length = 5
+    else:
+        length = 7
+    id = str(random())[2:(length+2)]
+    image = "https://i.imgur.com/" + id + "s.jpg" 
+    img = requests.get(image).url
+    if img != "https://i.imgur.com/removed.png":
+        imgList = img.strip()
+        print(imgList)
+        ret.append(imgList)
+    else:
+        randIMG(ret)
+    
+        
+def requestFast(itr):
+    threads = list()
+    rets = list()
+    for i in range(itr):
+        x = Thread(target=randIMG, args=(rets,))
+        threads.append(x)
+        x.start()
+    for index, thread in enumerate(threads):
+        thread.join()
+
+    return rets
+
+#requestFast(100)
+#input()
